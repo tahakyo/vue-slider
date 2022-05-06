c92725847e543a1e02264118c3a0d99d813617ca
tahakyo <ghazouani1988@email.com>
2022-05-06 19:51:47 +0200

    add image in html using vfor

--- js/script.js
+++ js/script.js
@@ -4,31 +4,31 @@ const app = new Vue (
         data : {
             countries: [
                 {
-                    image: '/img/01.jpg',
-                    title: 'svezia',
+                    image: 'img/01.jpg',
+                    title: 'Svezia',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
                 },
                 {
-                    image: '/img/02.jpg',
+                    image: 'img/02.jpg',
                     title: 'Svizzera',
-                    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
                 },
                 {
-                    image: '/img/03.jpg',
+                    image: 'img/03.jpg',
                     title: 'Gran Bretagna',
                     text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
                 },
                 {
-                    image: '/img/04.jpg',
+                    image: 'img/04.jpg',
                     title: 'Germania',
                     text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
                 },
                 {
-                    image: '/img/05.jpg',
+                    image: 'img/05.jpg',
                     title: 'Paradise',
                     text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
-                }
-            ]
+                },
+            ],
 
         },
         methods : {
