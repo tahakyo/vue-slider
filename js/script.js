73499297ab27e54e5a41f55f1d1a83a998406e22
tahakyo <ghazouani1988@email.com>
2022-05-06 17:31:06 +0200

    add countries array in data

--- js/script.js
+++ js/script.js
@@ -2,6 +2,33 @@ const app = new Vue (
     {
         el : '#root',
         data : {
+            countries: [
+                {
+                    image: '/img/01.jpg',
+                    title: 'svezia',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                },
+                {
+                    image: '/img/02.jpg',
+                    title: 'Svizzera',
+                    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+                },
+                {
+                    image: '/img/03.jpg',
+                    title: 'Gran Bretagna',
+                    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+                },
+                {
+                    image: '/img/04.jpg',
+                    title: 'Germania',
+                    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+                },
+                {
+                    image: '/img/05.jpg',
+                    title: 'Paradise',
+                    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+                }
+            ]
 
         },
         methods : {
